73bfe91a856ff15027d3c2d48a290bdae5143645
luhoro <luisashorowitz@gmail.com>
2024-06-04 10:56:50 -0300

    Refactor: states with usereducer

--- src/components/ItemCarrinho/index.jsx
+++ src/components/ItemCarrinho/index.jsx
@@ -3,10 +3,9 @@ import Botao from "@/components/Botao"
 import Quantidade from "@/components/Quantidade"
 import ValorFormatado from "@/components/ValorFormatado"
 import InfoItemCarrinho from "./InfoItemCarrinho"
+import { useCarrinhoContext } from "@/hooks/useCarrinhoContext"
 
-const ItemCarrinho = ({
-  itemCarrinho,
-}) => {
+const ItemCarrinho = ({ itemCarrinho }) => {
   const { adicionarProduto, removerProduto, removerProdutoCarrinho } =
     useCarrinhoContext()
 
